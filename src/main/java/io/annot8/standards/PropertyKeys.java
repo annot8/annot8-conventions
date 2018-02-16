8431fef91ebd952174c55496e656e1fe17252acd
James Baker <james.d.baker@gmail.com>
2018-02-16 13:47:10 +0000

    Alternative approach to defining standard values, with a number of suggested types (not complete)

---
+++ src/main/java/io/annot8/standards/PropertyKeys.java
@@ -0,0 +1,19 @@
+package io.annot8.standards;
+
+public class PropertyKeys {
+  private PropertyKeys(){
+    //No constructor - only access to public methods
+  }
+
+  public static final String PROPERTY_KEY_END = "end";
+  public static final String PROPERTY_KEY_FIRSTNAME = "firstname";
+  public static final String PROPERTY_KEY_GENDER = "gender";
+  public static final String PROPERTY_KEY_IDENTIFIER = "identifier";
+  public static final String PROPERTY_KEY_NAME = "name";
+  public static final String PROPERTY_KEY_NATIONALITY = "nationality";
+  public static final String PROPERTY_KEY_REGISTRATION = "registration";
+  public static final String PROPERTY_KEY_START = "start";
+  public static final String PROPERTY_KEY_SURNAME = "surname";
+  public static final String PROPERTY_KEY_TITLE = "title";
+  public static final String PROPERTY_KEY_VALUE = "value";
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,5 @@
 @SuppressWarnings("module")
-    module io.annot8.standard {
+module io.annot8.standard {
   requires io.annot8.core;
-  exports io.annot8.standards.properties;
-  exports io.annot8.standards.roles;
-  exports io.annot8.standards.types;
+  exports io.annot8.standards;
 }
