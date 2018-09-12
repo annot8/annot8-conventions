b722e0dadafe460c69716306e0240fba6ce65cb8
James Baker <james.d.baker@gmail.com>
2018-09-12 10:55:44 +0100

    Add quantities

--- src/main/java/io/annot8/conventions/AnnotationTypes.java
+++ src/main/java/io/annot8/conventions/AnnotationTypes.java
@@ -6,17 +6,21 @@ package io.annot8.conventions;
 public class AnnotationTypes {
 
   public static final String ANNOTATION_TYPE_ADDRESS = "address";
+  public static final String ANNOTATION_TYPE_AREA = "area";
   public static final String ANNOTATION_TYPE_CONCEPT = "concept";
   public static final String ANNOTATION_TYPE_COORDINATE = "coordinate";
   public static final String ANNOTATION_TYPE_CURRENCY = "currency";
+  public static final String ANNOTATION_TYPE_DISTANCE = "distance";
   public static final String ANNOTATION_TYPE_DOCUMENT = "document";
   public static final String ANNOTATION_TYPE_DOMAIN = "domain";
   public static final String ANNOTATION_TYPE_EMAIL = "email";
   public static final String ANNOTATION_TYPE_FINANCIALACCOUNT = "financialaccount";
+  public static final String ANNOTATION_TYPE_FREQUENCY = "frequency";
   public static final String ANNOTATION_TYPE_GEOPOLITICALENTITY = "gpe";
   public static final String ANNOTATION_TYPE_IPADDRESS = "ipaddress";
   public static final String ANNOTATION_TYPE_HASHTAG = "hashtag";
   public static final String ANNOTATION_TYPE_LOCATION = "location";
+  public static final String ANNOTATION_TYPE_MASS = "mass";
   public static final String ANNOTATION_TYPE_MONEY = "money";
   public static final String ANNOTATION_TYPE_ORGANISATION = "organisation";
   public static final String ANNOTATION_TYPE_PASSPORT = "passport";
@@ -26,7 +30,9 @@ public class AnnotationTypes {
   public static final String ANNOTATION_TYPE_URL = "url";
   public static final String ANNOTATION_TYPE_USERNAME = "username";
   public static final String ANNOTATION_TYPE_VEHICLE = "vehicle";
+  public static final String ANNOTATION_TYPE_VOLUME = "volume";
   public static final String ANNOTATION_TYPE_WEAPON = "weapon";
+
   private AnnotationTypes() {
     //No constructor - only access to public methods
   }
